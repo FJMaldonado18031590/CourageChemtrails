44cf9578614fdea2bbb493c486d45659c5afaebf
Moises Ismael Reyes Garcia <60557986+MoisesReyes117@users.noreply.github.com>
2021-05-07 00:41:26 -0500

    Creamos la carpeta base del programa Bowling

---
+++ BowlingGame/src/test/java/com/example/BowlingGameTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import com.example.BowlingGame.BowlingGame;
+import org.junit.Test;
+
+public class BowlingGameTest {
+    @Test
+    public void testGutterGame() throws Exception {
+        BowlingGame bowlingGame = new BowlingGame();
+    }
+}
