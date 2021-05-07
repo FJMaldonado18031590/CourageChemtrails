7dfc0b4e96766e8a6161e120177f5072af211d03
FJMaldonado18031590 <18031590@itcelaya.edu.mx>
2021-05-07 01:24:10 -0500

    Agregando Primer Prueba
    Se agrego la primer prueba

---
+++ Parser/Test/Main/EcuacionPrimerGradoIntegrationTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EcuacionPrimerGradoIntegrationTest
+{
+    EcuacionPrimerGrado ecuacion = new EcuacionPrimerGrado();
+
+    @Test
+    public void solucionaEcuacionConMenos() {
+
+        Double result = ecuacion.obtenerResultado("2x - 1 = 0");
+
+        Double valueExpected = 0.5;
+
+        assertEquals(valueExpected, result);
+    }
+
+    @Test
+    public void solucionaEcuacionConMas() {
+
+        Double result = ecuacion.obtenerResultado("2x + 1 = 0");
+
+        Double valueExpected = -0.5;
+
+        assertEquals(valueExpected, result);
+    }
+
+    @Test
+    public void solucionaEcuacionConParte3Mayor0() {
+
+        Double result = ecuacion.obtenerResultado("2x + 1 = 10");
+
+        Double valueExpected = 4.5;
+
+        assertEquals(valueExpected, result);
+    }
+
+    @Test
+    public void solucionaEcuacionConParte3Menor0() {
+
+        Double result = ecuacion.obtenerResultado("2x + 1 = -10");
+
+        Double valueExpected = -5.5;
+
+        assertEquals(valueExpected, result);
+    }
+}
