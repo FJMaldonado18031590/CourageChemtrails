e85aafadb390aad58c6f855cca395aecd17937e7
Moises Ismael Reyes Garcia <60557986+MoisesReyes117@users.noreply.github.com>
2021-05-11 13:46:53 -0500

    Implementacion del Spare

--- BowlingGame/src/test/java/com/example/BowlingGameTest.java
+++ BowlingGame/src/test/java/com/example/BowlingGameTest.java
@@ -17,21 +17,30 @@ public class BowlingGameTest {
 
     @Test
     public void testGutterGame() {
-
-        for (int i = 0; i < 20; i++) {
-            bowlingGame.roll(0);
-
+        rollMany(20,0);
             assertEquals(0, bowlingGame.score());
-        }
+
     }
 
     @Test
     public void testAllOnes()  {
-        for(int i=0;i<20;i++){
-            bowlingGame.roll(1);
-        }
+        rollMany(20,1);
         assertEquals(20,bowlingGame.score());
+    }
 
+    @Test
+    public void testOneSpare(){
+        bowlingGame.roll(5);
+        bowlingGame.roll(5);
+        bowlingGame.roll(3);
+        rollMany(17,0);
+        assertEquals(16,bowlingGame.score());
+    }
+
+    private void rollMany(int n, int pins){
+        for (int i = 0; i < n; i++) {
+            bowlingGame.roll(pins);
+        }
     }
 
 }
