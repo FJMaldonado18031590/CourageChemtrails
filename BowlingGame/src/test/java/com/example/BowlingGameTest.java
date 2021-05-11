b7a77fd276c82c220f0cbfb1d6cf7a536168bb0c
Marlenec18 <mar_halo@hotmail.com>
2021-05-10 20:36:28 -0500

    creacion nuevas pruebas

--- BowlingGame/src/test/java/com/example/BowlingGameTest.java
+++ BowlingGame/src/test/java/com/example/BowlingGameTest.java
@@ -1,17 +1,37 @@
 package com.example;
 
 import com.example.BowlingGame.BowlingGame;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class BowlingGameTest {
+
+    private BowlingGame bowlingGame;
+
+    @Before
+    public void setUp() {
+        bowlingGame = new BowlingGame();
+    }
+
     @Test
-    public void testGutterGame() throws Exception {
-        BowlingGame bowlingGame = new BowlingGame();
+    public void testGutterGame() {
+
         for (int i = 0; i < 20; i++) {
             bowlingGame.roll(0);
+
             assertEquals(0, bowlingGame.score());
         }
     }
+
+    @Test
+    public void testAllOnes()  {
+        for(int i=0;i<20;i++){
+            bowlingGame.roll(1);
+        }
+        assertEquals(20,bowlingGame.score());
+
+    }
+
 }
